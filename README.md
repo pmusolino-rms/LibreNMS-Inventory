0791395037171d0fd38f621e5f0ff26935e4a255
Peter Musolino <pmusolino@rms.com>
2018-08-27 12:02:45 +0530

    Import files

---
+++ README.md
@@ -0,0 +1,10 @@
+# LibreNMS-Inventory
+
+## Required packages
+- iPython
+- pivottablejs
+- pandas
+
+## LibreNMS token
+1. Create an API access token from https://librenms.example.com/api-access/
+2. Copy your token hash to secrets.py
---
+++ get_inventory.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import requests
+import os
+import json
+import sys
+import csv
+import pandas
+from IPython.core.display import display, HTML
+from pivottablejs import pivot_ui
+from pprint import pprint
+from secrets import token
+
+API_HOST="librenms-api.example.com"
+API_ROOT="/api"
+API_VERSION="/v0"
+API_URI="https://" + API_HOST + API_ROOT + API_VERSION
+CA_BUNDLE='./bundle.trust.crt'
+CSV_FILE='/tmp/dev.csv'
+HTML_FILE='/opt/librenms/html/device_inventory.html'
+NMS_DEVICE_URL=API_HOST + "/device/device="
+RANCID="rancid-host.example.com/repo/src/master/configs/"
+if __name__ == '__main__':
+    devices_list = []
+    device_dict = {}
+    headers = { 
+        'X-Auth-Token': token
+        }
+    devices_uri=API_URI + "/devices"
+    r = requests.get(devices_uri, headers=headers,verify=False)
+    if (r.ok):
+        json_device_list = r.json()
+    else:
+        print("Unable to fetch data")
+        sys.exit(1)
+
+    for i in json_device_list['devices']:
+        device_dict['sysName'] = i['sysName']
+        device_dict['ip'] = i['ip']
+        device_dict['sysDescr'] = i['sysDescr']
+        device_dict['os'] = i['os']
+        device_dict['version'] = i['version']
+        device_dict['location'] = i['location']
+        device_dict['hardware'] = i['hardware']
+        device_dict['hostname'] = i['hostname']
+        device_dict['features'] = i['features']
+        device_dict['serial'] = i['serial']
+        device_dict['nms'] = "https://" + NMS_DEVICE_URL + str(i['device_id'])
+        device_dict['rancid'] = "http://" + RANCID + i['hostname']
+        devices_list.append(dict(device_dict))
+
+with open(CSV_FILE, 'w') as csvfile:
+    writer = csv.DictWriter(csvfile,delimiter=',',fieldnames=devices_list[0].keys())
+    writer.writeheader()
+    writer.writerows(devices_list)
+
+df = pandas.read_csv(CSV_FILE)
+pivot_ui(df,outfile_path=HTML_FILE,rows=['sysName','hostname','ip','hardware','os','version','features','serial','location','nms','rancid'])
